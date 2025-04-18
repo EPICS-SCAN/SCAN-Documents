9bcf8fed5ef59320ee518d871e5b884d890be526
unknown <chris.sojka1@gmail.com>
2025-04-18 18:58:29 -0400

    Done

--- object_split.py
+++ object_split.py
@@ -2,11 +2,13 @@ import csv
 
 class ObjectSplit():
 
-    def __init__(self, file_input: str, file_output: str) -> None:
+    def __init__(self, file_input: str, file_output: str, file_output_adv: str) -> None:
         if not isinstance(file_input, str): 
             raise Exception("File input must be a string")
         if not isinstance(file_output, str):
             raise Exception("File output must be a string")
+        if not isinstance(file_output_adv, str):
+            raise Exception("File output adv must be a string")
         
         try:
             with open(file_input, 'r') as file:
@@ -17,6 +19,7 @@ class ObjectSplit():
         
         self.file_input = file_input
         self.file_output = file_output
+        self.file_output_adv = file_output_adv
 
     def ds(self):
         open(self.file_output, "w").close()
@@ -72,8 +75,36 @@ class ObjectSplit():
                     end_count += 1
                     with open(self.file_output, "a") as file2:
                         file2.write(f"{curr_time},{curr_distance}\n")
+        
+    def ds_adv(self):
+        open(self.file_output_adv, "w").close()
+        with open(self.file_output, "r") as file:
+            content = csv.reader(file)
+            high = True
+            for lines in content:
+                if lines == ["---"]:
+                    continue
+                if len(lines) != 2:
+                    raise Exception("Input file is not in the format: Time,Distance")
+                try:
+                    lines[0] = int(lines[0])
+                    lines[1] = int(lines[1])
+                except ValueError:
+                    raise Exception("Input file is not in the format: Time,Distance")
+            
+                if lines[1] < 190:
+                    with open(self.file_output_adv, "a") as file2:
+                        file2.write(f'{lines[0]},{lines[1]}\n')
+                        high = False
+                elif not high:
+                     with open(self.file_output_adv, "a") as file2:
+                        file2.write("---\n")
+                        high = True
+
+
 
 if __name__ == "__main__":
-    test = ObjectSplit("RAK_DATA.TXT", "RAK_DATA_SPLIT.txt")
-    test.ds()
+    test = ObjectSplit("RAK_DATA.TXT", "RAK_DATA_SPLIT.txt", "RAK_DATA_SPLIT_ADV.txt")
+    #test.ds()
+    test.ds_adv()
     
